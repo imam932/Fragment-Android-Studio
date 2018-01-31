6eb93b4e2b7216560c4f4b1aed6073f684c7eb65
imam <nawawi932@gmail.com>
2018-01-31 10:25:26 +0700

    add layout Fragment

--- app/src/main/java/id/nawawi/layouting_with_fragment/MainActivity.java
+++ app/src/main/java/id/nawawi/layouting_with_fragment/MainActivity.java
@@ -2,12 +2,24 @@ package id.nawawi.layouting_with_fragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_fragment = (Button) findViewById(R.id.btn_fragment);
+        btn_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_layout, new TampilanFragment()).commit();
+            }
+        });
     }
 }
--- app/src/main/java/id/nawawi/layouting_with_fragment/TampilanFragment.java
+++ app/src/main/java/id/nawawi/layouting_with_fragment/TampilanFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class TampilanFragment extends Fragment {
 
+    Button btn_show;
 
     public TampilanFragment() {
         // Required empty public constructor
@@ -23,7 +26,17 @@ public class TampilanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tampilan, container, false);
+        View view = inflater.inflate(R.layout.fragment_tampilan, container, false);
+
+        btn_show = (Button) view.findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "This is from Fragment !", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
 }
